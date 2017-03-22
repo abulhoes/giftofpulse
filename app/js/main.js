1a4835c107f7d113191fdc700adf87472d2492f3
aliciabulhoes <aliciabulhoes@gmail.com>
2017-03-22 14:48:54 -0400

    New main.js file with the arduino code

--- app/js/main.js
+++ app/js/main.js
@@ -20,50 +20,100 @@ process.__defineGetter__('stdin', function() {
 
 var board = new five.Board();
 
- board.on("ready", function()  {
+board.on("ready", function()  {
 
 var display = document.querySelector("#display");
-var led = new five.Led("13");
-var led2 = new five.Led("9");
 
-var sensorOne = new five.Sensor({
-   pin:"A0",
-   freq:250
- });
-var sensorTwo = new five.Sensor({
-   pin:"A2",
-   freq:250
- });
+var blueLed1 = new five.Led("13");
+var redLed1 = new five.Led("5");
+var blueLed2 = new five.Led("3");
+var redLed2 = new five.Led("6");
 
-sensorOne.on("change", function(){
-  console.log(this.value);
-  if(sensorOne.value < 100){
-    led.on();
-  } else {
-    led.off();
-  }
+var touch = new five.Button(9);
+var touch2 = new five.Button(10);
+// var sensorThree = new five.Button()
+// var sensorFour = new five.Button()
 
-sensorTwo.on("change", function(){
-  console.log(this.value);
-  if(sensorTwo.value < 100){
-    led2.on(); 
-  } else {
-    led2.off();
-  }
-});
+  touch.on("hold", function(){
+    redLed1.pulse();
+    blueLed1.blink();
+     
+  });
 
-if(sensorOne.value < 100 && sensorTwo.value < 100) { 
-   display.innerHTML = "16 lives";
-} else if(sensorOne.value < 100 && sensorTwo.value > 100) {
-  display.innerHTML="8 Lives";
-} else if(sensorOne.value > 100 && sensorTwo.value < 100) {
-  display.innerHTML="8 Lives";
-} else {
-  display.innerHTML="";
-}
+  touch.on("release", function(){
+     blueLed1.stop();
+      redLed1.stop();
+      blueLed1.off();
+       redLed1.off();
+  });
+
+  touch2.on("hold", function(){
+    redLed2.pulse();
+    blueLed2.blink();
+     
+  });
+
+  touch2.on("release", function(){
+     blueLed2.stop();
+      redLed2.stop();
+      blueLed2.off();
+       redLed2.off();
+  });
 
 
-});//closing sensorOne
+
+
+// if(sensorOne.value < 100 && sensorTwo.value < 100 && sensorThree.value < 100 && sensorfour.value < 100) { 
+//    display.innerHTML = "32 lives";
+// } else if(sensorOne.value < 100 && sensorTwo.value < 100 && sensorThree.value < 100 && sensorfour.value > 100) {
+//   display.innerHTML="24 Lives";
+// else if(sensorOne.value < 100 && sensorTwo.value < 100 && sensorThree.value > 100 && sensorfour.value < 100) {
+//   display.innerHTML="24 Lives";
+// }
+// else if(sensorOne.value < 100 && sensorTwo.value > 100 && sensorThree.value < 100 && sensorfour.value < 100) {
+//   display.innerHTML="24 Lives";
+// }
+// else if(sensorOne.value > 100 && sensorTwo.value < 100 && sensorThree.value < 100 && sensorfour.value < 100) {
+//   display.innerHTML="24 Lives";
+// }
+// else if(sensorOne.value < 100 && sensorTwo.value < 100 && sensorThree.value > 100 && sensorfour.value > 100) {
+//   display.innerHTML="16 Lives";
+// }
+// else if(sensorOne.value < 100 && sensorTwo.value > 100 && sensorThree.value > 100 && sensorfour.value < 100) {
+//   display.innerHTML="16 Lives";
+// }
+// else if(sensorOne.value > 100 && sensorTwo.value > 100 && sensorThree.value < 100 && sensorfour.value < 100) {
+//   display.innerHTML="16 Lives";
+
+// }
+// else if(sensorOne.value > 100 && sensorTwo.value < 100 && sensorThree.value < 100 && sensorfour.value > 100) {
+//   display.innerHTML="16 Lives";
+// }
+// else if(sensorOne.value > 100 && sensorTwo.value < 100 && sensorThree.value > 100 && sensorfour.value < 100) {
+//   display.innerHTML="16 Lives";
+// }
+// else if(sensorOne.value < 100 && sensorTwo.value > 100 && sensorThree.value < 100 && sensorfour.value > 100) {
+//   display.innerHTML="16 Lives";
+// }
+// if(sensorOne.value < 100 && sensorTwo.value > 100 && sensorThree.value > 100 && sensorfour.value > 100) { 
+//    display.innerHTML = "8 lives";
+// } 
+// if(sensorOne.value > 100 && sensorTwo.value < 100 && sensorThree.value > 100 && sensorfour.value > 100) { 
+//    display.innerHTML = "8 lives";
+// } 
+// if(sensorOne.value > 100 && sensorTwo.value > 100 && sensorThree.value < 100 && sensorfour.value > 100) { 
+//    display.innerHTML = "8 lives";
+// } 
+// if(sensorOne.value > 100 && sensorTwo.value > 100 && sensorThree.value > 100 && sensorfour.value < 100) { 
+//    display.innerHTML = "8 lives";
+// } 
+// } else {
+//   display.innerHTML="";
+// }
+
+
+
+//});//closing sensorOne
 
 
 
@@ -72,4 +122,3 @@ if(sensorOne.value < 100 && sensorTwo.value < 100) {
 // display();
 
 })();
-
